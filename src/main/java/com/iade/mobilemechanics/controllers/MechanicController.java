f2dd05ef57f665c469ea6f260aec4cd02c9b83a4
jonhot <jonhot111@gmail.com>
2021-11-02 20:59:18 +0000

    commit

--- src/main/java/com/iade/mobilemechanics/controllers/MechanicController.java
+++ src/main/java/com/iade/mobilemechanics/controllers/MechanicController.java
@@ -1,4 +1,9 @@
 package com.iade.mobilemechanics.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/")
 public class MechanicController {
 }
