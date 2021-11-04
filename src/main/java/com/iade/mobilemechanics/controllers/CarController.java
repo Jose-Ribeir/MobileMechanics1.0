12a967efde788afc48748abc27b708dd5ce51416
Jose-Ribeir <jopirulim@gmail.com>
2021-11-04 19:04:23 +0000

    upupdate

--- src/main/java/com/iade/mobilemechanics/controllers/CarController.java
+++ src/main/java/com/iade/mobilemechanics/controllers/CarController.java
@@ -1,9 +1,16 @@
 package com.iade.mobilemechanics.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/")
+@RequestMapping(path = "/api/cars")
 public class CarController {
+    @GetMapping(path = "/hello",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String sayhello(){
+        return "hello jaimie";
+        
+    }
 }
