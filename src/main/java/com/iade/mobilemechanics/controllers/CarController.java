391a47e8383fa4a70515c4384a3957f2c2bd1efe
Jose-Ribeir <jopirulim@gmail.com>
2021-11-04 23:28:14 +0000

    upupupdate
    carcontroller

--- src/main/java/com/iade/mobilemechanics/controllers/CarController.java
+++ src/main/java/com/iade/mobilemechanics/controllers/CarController.java
@@ -1,20 +1,46 @@
 package com.iade.mobilemechanics.controllers;
 
+import com.iade.mobilemechanics.models.Car;
+import com.iade.mobilemechanics.models.repositories.CarRepository;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/cars")
 public class CarController {
-    @GetMapping(path = "/hello",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String sayhello(){
-        return "hello jaimie";
-    }
-    @GetMapping(path = "/goodbye",produces = MediaType.APPLICATION_JSON_VALUE)
-    public String saygoodbye(){
-        return "bye jaimie";
+    private final Logger logger = LoggerFactory.getLogger(CarController.class);
 
+
+
+    @Autowired
+    private CarRepository carRepository;
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Car> getCars(){
+        logger.info("Send all cars to Request");
+        return carRepository.findAll();
+    }
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Optional<Car> getcar(@PathVariable int id){
+        logger.info("Send car with id "+ id + "to Request");
+        return carRepository.findById(id);
+    }
+    @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Car saveCar(@RequestBody Car car){
+        Car savedCar = carRepository.save(car);
+        logger.info("Save car id " + savedCar.getId() + " to Database");
+        return savedCar;
+    }
+
+    @DeleteMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String deletecar(@PathVariable int id){
+        logger.info("Delete car with id "+ id + "to Request");
+        carRepository.deleteById(id);
+        return "Deleted";
     }
 }
