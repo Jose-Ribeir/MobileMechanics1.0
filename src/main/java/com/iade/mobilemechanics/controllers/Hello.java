0150f5ae82fae0ca04eb56adda9790402aeddfcb
jonhot <jonhot111@gmail.com>
2021-11-05 18:57:01 +0000

    commit say bye

--- src/main/java/com/iade/mobilemechanics/controllers/Hello.java
+++ src/main/java/com/iade/mobilemechanics/controllers/Hello.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCars(){
-        return "hello1";
+        return "hello2";
     }
 }
