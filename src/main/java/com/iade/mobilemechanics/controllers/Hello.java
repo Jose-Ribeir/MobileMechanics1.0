4b22dcc2671dd9ded5168827bc1b1285762dcab1
jonhot <jonhot111@gmail.com>
2021-11-05 19:14:57 +0000

    commit say bye

--- src/main/java/com/iade/mobilemechanics/controllers/Hello.java
+++ src/main/java/com/iade/mobilemechanics/controllers/Hello.java
@@ -3,6 +3,7 @@ package com.iade.mobilemechanics.controllers;
 
 import com.iade.mobilemechanics.models.Car;
 import com.iade.mobilemechanics.models.repositories.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/hello")
 public class Hello {
+    @Autowired
+    private CarRepository carRepository;
+    @GetMapping(path = "/h", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<Car> getcar(){
+        return carRepository.findAll();
+    }
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCars(){
-        return "hello2";
+        return "hello3";
     }
     @GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCars2(){
         return "hello2";
     }
 }
+
