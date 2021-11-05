dcf586f05cb35915ed5eb2274441c929d3abfe7c
jonhot <jonhot111@gmail.com>
2021-11-05 18:42:17 +0000

    commit say bye

---
+++ src/main/java/com/iade/mobilemechanics/controllers/Hello.java
@@ -0,0 +1,18 @@
+package com.iade.mobilemechanics.controllers;
+
+
+import com.iade.mobilemechanics.models.Car;
+import com.iade.mobilemechanics.models.repositories.CarRepository;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/hello")
+public class Hello {
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getCars(){
+        return "hello";
+    }
+}
