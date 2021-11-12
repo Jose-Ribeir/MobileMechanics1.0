f25638ef7c50b3a4602598bde048f4b5c124e5a8
jonhot <jonhot111@gmail.com>
2021-11-12 10:26:58 +0000

    commit

--- src/main/java/com/iade/mobilemechanics/models/Model.java
+++ src/main/java/com/iade/mobilemechanics/models/Model.java
@@ -5,7 +5,6 @@ import javax.persistence.*;
 @Entity
 @Table(name="models")
 public class Model {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "model_id", nullable = false)
@@ -13,6 +12,17 @@ public class Model {
 
     @Column(name = "model_name", length = 69)
     private String modelName;
+
+    @Column(name = "model_brand_id")
+    private Integer modelBrandId;
+
+    public Integer getModelBrandId() {
+        return modelBrandId;
+    }
+
+    public void setModelBrandId(Integer modelBrandId) {
+        this.modelBrandId = modelBrandId;
+    }
 
     public String getModelName() {
         return modelName;
--- src/main/java/com/iade/mobilemechanics/models/repositories/EngineRepository.java
+++ src/main/java/com/iade/mobilemechanics/models/repositories/EngineRepository.java
@@ -4,4 +4,5 @@ import com.iade.mobilemechanics.models.Engine;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EngineRepository extends CrudRepository<Engine, Integer> {
+
 }
