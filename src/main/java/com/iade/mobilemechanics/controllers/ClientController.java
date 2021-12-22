5ffe93be452fa87528dbc4646b30be4fbb0bbc64
José R <jopirulim@gmail.com>
2021-12-22 18:17:12 +0000

    Repair com put de data

--- src/main/java/com/iade/mobilemechanics/controllers/ClientController.java
+++ src/main/java/com/iade/mobilemechanics/controllers/ClientController.java
@@ -1,8 +1,13 @@
 package com.iade.mobilemechanics.controllers;
 
+import com.iade.mobilemechanics.models.Brand;
+import com.iade.mobilemechanics.models.Car;
 import com.iade.mobilemechanics.models.Client;
+import com.iade.mobilemechanics.models.Person;
 import com.iade.mobilemechanics.models.exceptions.NotFoundException;
 import com.iade.mobilemechanics.models.repositories.ClientRepository;
+import com.iade.mobilemechanics.models.repositories.PersonRepository;
+import com.iade.mobilemechanics.models.request.ClientRequeste;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +23,13 @@ public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
+    private PersonRepository personRepository;
+
+    public ClientController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<Client> getClients(){
         logger.info("Send all Clients to Request");
@@ -44,9 +56,18 @@ public class ClientController {
         //dsadsa
     }
 
+
+
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Client saveClient(@RequestBody Client client){
-        Client saveClient = clientRepository.save(client);
+    public Client saveClientforeingkey(@RequestBody ClientRequeste client){
+
+        Optional<Person> person = personRepository.findById(client.getClientPerson());
+        Client client1 = new Client();
+        client1.setClientNif(String.valueOf(client.getClientNif()));
+        client1.setClientPerson(person.get());
+
+        Client saveClient = clientRepository.save(client1);
+
         logger.info("Save Client id " + saveClient.getId() + " to Database");
         return saveClient;
     }
---
+++ src/main/java/com/iade/mobilemechanics/models/request/ClientRequeste.java
@@ -0,0 +1,22 @@
+package com.iade.mobilemechanics.models.request;
+
+public class ClientRequeste {
+    private int clientNif;
+    private int clientPerson;
+
+    public int getClientNif() {
+        return clientNif;
+    }
+
+    public void setClientNif(int clientNif) {
+        this.clientNif = clientNif;
+    }
+
+    public int getClientPerson() {
+        return clientPerson;
+    }
+
+    public void setClientPerson(int clientPerson) {
+        this.clientPerson = clientPerson;
+    }
+}
